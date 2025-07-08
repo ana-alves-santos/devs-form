aa5a69564747d77995ea40d24326d8cc2c165f4e
Ana <113148426+ana-alves-santos@users.noreply.github.com>
2025-07-07 21:25:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const form = document.querySelector('form');
 const mensagem = document.getElementById('mensagem');
 
 form.addEventListener('submit', function(event) {
-  event.preventDefault(); // evita envio real da página
+  event.preventDefault(); 
 
-  // Esconde o formulário
+
   form.style.display = 'none';
 
-  // Mostra a mensagem de obrigado
+  
   mensagem.style.display = 'block';
 });
